7a8fb7e71050a3aa6c9a27ca5aad896999d2c996
david-braga <bragav.david@gmail.com>
2022-04-13 20:30:49 +1200

    fix interface props

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,27 @@ import family from './data';
 import 'react-tree-graph/dist/style.css'
 import './App.css';
 
-// interface Family {
-//   id: number;
-//   name: string;
-//   children: number[];
-//   gender: string;
-//   parents: number[];
-//   partner?: {
-//     id: number,
-//     name: string,
-//     gender: string,
-//   },
-// }
+interface Family {
+  id: number;
+  name: string;
+  children?: number[];
+  gender?: string;
+  parents?: number[];
+}
 
-const App: React.FC = (props: any) => {
+const App = (props: Family) => {
 
   return (
-    <div className='container'>
+
+    <div className='App' >
       <Tree
         animated={true}
         data={family}
         nodeRadius={15}
         margins={{ top: 20, bottom: 10, left: 20, right: 200 }}
         height={700}
-        width={1000} />
-    </div>
+        width={1000} >{props.name}</Tree>
+    </div >
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App id={0} name={''} />
   </React.StrictMode>,
   document.getElementById('root')
 );
